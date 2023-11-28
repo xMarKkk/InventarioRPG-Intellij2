137d47affa87bd3a71e41bffaae93161cc98fdb6
xMarKkk <148650610+xMarKkk@users.noreply.github.com>
2023-11-28 00:29:37 +0000

    Versão classificada corretamente
    .

--- src/Inventario.java
+++ src/Inventario.java
@@ -9,7 +9,40 @@ public class Inventario {
 
     //variaveis que agrupam os slots do inventario;
     int numeroDoSlot = 10; //Vai indicar em qual slot está e qual o limite da array;
-    int numeroDoItem = 30; //Vai indicar qual item é pela enumeração e tambem definir limite do array
+    int numeroDoItem = 24; //Vai indicar qual item é pela enumeração e tambem definir limite do array
     int[][] slots = new int[numeroDoSlot][numeroDoItem];
 
+  
+  public Inventario(){
+    slots[0][3] = 3;
+    slots[9][22] = 1;
+  }
+
+  
+  public void exibirInventario(){
+    //Exibe a quantidade de itens, o tipo e em qual slot o item esta:
+    for(int i=0; i<numeroDoItem; i++){
+        for(int j=0; j<numeroDoSlot; j++){
+            if(slots[j][i]>0) {
+                System.out.println("Slot "+(j+1)+" tem "+slots[j][i]+" itens do tipo "+(i+1));
+            }
+            else if(slots[j][i]==0){
+                continue;
+            }
+        }
+    }
+
+  }
+
+
+  public void exibirMapaInv(){
+    //Exibe um "mapa" do seu inventario, onde as linhas são os slots e colunas os itens e o valor é a quantidade:
+    for(int[] slot : slots){
+        for(int slot1 : slot){
+                System.out.print(slot1 + " ");
+        }
+        System.out.println();
+    }
+  }
 }
+//slot[i][0] = Adaga();
--- src/Pocoes.java
+++ src/Pocoes.java
@@ -10,4 +10,43 @@ public class Pocoes {
   Poção de Veneno = pocao[4];
 
   */
+
+  public Pocoes(){
+    pocao[0] = 7;
+    pocao[1] = 1;
+    pocao[2] = 0;
+    pocao[3] = 2;
+    pocao[4] = 2;
+  }
+  
+  public void exibirPocoes(){
+    //Exibe quais poções você tem:
+      for(int i=0; i<5; i++){
+      if(pocao[i]>0){
+          System.out.print("Você tem "+pocao[i]+" poções");
+          if(i==0){
+              System.out.println(" de Cura");
+              continue;
+          }
+          else if(i==1){
+              System.out.println(" de Mana");
+              continue;
+          }
+          else if(i==2){
+              System.out.println(" de Invisibilidade");
+              continue;
+          }
+          else if(i==3){
+              System.out.println(" de Resistencia ao Fogo");
+              continue;
+          }
+          else if(i==4){
+              System.out.println(" de Veneno");
+          }
+      }
+      else if(pocao[i]==0){
+          System.out.println("Você não tem poções do tipo "+(i+1));
+      }
+    }
+  }
 }
